d422cc0f15c568c548da91791bcc68852fe3e946
Piyush <piyushgadge800@gmail.com>
2022-08-11 10:43:52 +0530

    almost done

--- lib/utils/global_var.dart
+++ lib/utils/global_var.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post.dart';
 import 'package:instagram_flutter/screens/feed_screen.dart';
+import 'package:instagram_flutter/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('ss'),
+  SearchScreen(),
   AddPostScreen(),
   Text('daccta'),
   Text('daataaa'),
